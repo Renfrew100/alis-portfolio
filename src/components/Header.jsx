f28a823e781ac91aaeb8563fd3ce86b36a867a37
Ali Muhammad <aliaftab9@hotmail.com>
2023-08-11 13:30:13 -0400

    modified URLs on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,10 +9,10 @@ export const Header = ({ home, projects, workexperience, about, resume }) => (
             {(
                 <>
                 <a href="/"><Button onClick = {home} size="large" label="Home" /></a>
-                <a href="/projects"><Button onClick = {projects} size="large" label="About" /></a>
-                <a href="/work experience"><Button onClick = {workexperience} size="large" label="Work Experience" /></a>
-                <a href="/about"><Button onclick = {about} size="large" label="Projects"/></a>
-                <a href="/resume"><Button onclick = {resume} size="large" label="My Resume"/></a>
+                <a href="/alis-portfolio/projects/"><Button onClick = {projects} size="large" label="About" /></a>
+                <a href="/alis-portfolio/work experience/"><Button onClick = {workexperience} size="large" label="Work Experience" /></a>
+                <a href="/alis-portfolio/about/"><Button onclick = {about} size="large" label="Projects"/></a>
+                <a href="/alis-portfolio/resume/"><Button onclick = {resume} size="large" label="My Resume"/></a>
                 </>
             )}
     </header>
